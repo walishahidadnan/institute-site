215cc9b775bab3ec7d7b2c59535b7cc0e0e61444
Shahid Adnan Wali <walishahidadnan@gmail.com>
2024-01-18 12:06:39 +0500

    added and removed some files

---
+++ meidSite/src/Components/main.ts
@@ -0,0 +1,25 @@
+
+
+function OneTwoGO(name: string){
+    let one : number = 10;
+    let two: string = 'shahid';
+    
+    console.log(one, two)
+}
+
+// let myname:string = 'shahidadnan';
+// let Num = 5;
+// let Num2 = 7;
+
+// let Result:number = Num ^ Num2
+
+// console.log(Result, myname)
+
+function greet (a: string, b:number): string {
+    return `your userName is ${a} and id is ${b}`
+}
+
+let result:string = greet('shahid', 2)
+console.log(result)
+export {};
+
